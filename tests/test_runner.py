21986282f264b404facfadb778be2c7e6e70c5bf
Oshan Mudannayake <oshan.ivantha@gmail.com>
2019-12-28 10:43:26 +0530

    Tweak default test states

--- tests/test_runner.py
+++ tests/test_runner.py
@@ -20,26 +20,26 @@ if __name__ == '__main__':
         '../datasets/unicorn_wget/benign_base_100/',
     ]
 
-    buildtime_test(datasets)
-    buildtime_visualize()
+    # buildtime_test(datasets)
+    # buildtime_visualize()
 
-    are_test(datasets)
-    are_visualize()
+    # are_test(datasets)
+    # are_visualize()
 
-    neq_test(datasets)
-    neq_visualize()
+    # neq_test(datasets)
+    # neq_visualize()
 
-    dd_test(datasets)
-    dd_visualize()
+    # dd_test(datasets)
+    # dd_visualize()
 
-    ewd_test(datasets)
-    ewd_visualize()
+    # ewd_test(datasets)
+    # ewd_visualize()
 
-    dc_test(datasets)
-    dc_visualize()
+    # dc_test(datasets)
+    # dc_visualize()
 
-    hn_test(datasets)
-    hn_visualize()
+    # hn_test(datasets)
+    # hn_visualize()
 
-    he_test(datasets)
-    he_visualize()
+    # he_test(datasets)
+    # he_visualize()
