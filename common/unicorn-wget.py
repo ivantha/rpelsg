5bc974fce2958297d09d51e503cd2e3927b59edb
Oshan Mudannayake <oshan.ivantha@gmail.com>
2020-01-25 13:26:30 +0530

    Revamp preprocessors

--- common/unicorn-wget.py
+++ common/unicorn-wget.py
@@ -7,13 +7,15 @@ from google_drive_downloader import GoogleDriveDownloader as gdd
 
 from common.utils import get_txt_files
 
-parent_dir = '../datasets/email-EuAll'
-
 files = (
-    ('email-EuAll', '1HNw17l-PtdVgcMcWnCsZndGEWVjin3Oz'),
+    ('benign_base', '1-rmP2_vOgIU56husFP0AhHK3O3HjPi0d'),
+    ('benign_streaming', '1-urTwbWoqmeI-Y4Blhq1Xi_qnh2zDUhR'),
+    ('attack_base', '1-jcapoEm0lEri9sBHn5RvTkrnDvSo0QG'),
+    ('attack_streaming', '1-nZMkgFLfRPXCGKX2yo62ykSn2FGAh6X'),
 )
 
 if __name__ == '__main__':
+    parent_dir = os.path.basename(__file__).split('.')[0]
     os.makedirs(parent_dir, exist_ok=True)
 
     for file_base, drive_id in files:
@@ -65,7 +67,7 @@ if __name__ == '__main__':
             with open('{}/{}_{}/data.txt'.format(parent_dir, file_base, suffix), 'w') as new_f:
                 for line in reservoir:
                     try:
-                        source_id, target_id = line.split()
+                        source_id, target_id, _ = line.split(' ')
                         new_f.write('{},{}\n'.format(source_id, target_id))
                     except:
                         print('Error in line > {}'.format(line))
